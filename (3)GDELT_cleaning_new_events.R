3a30f854cd9f1a81390b0e1ead63abf02fcfd863
Yegor Baranovski <baranovski@wisc.edu>
2024-02-07 15:01:56 -0600

    scirpt to clean GDELT and add events v1,v2,v3,ECON,MIL

---
+++ (3)GDELT_cleaning_new_events.R
@@ -0,0 +1,54 @@
+
+library(data.table)
+library(dplyr)
+library(readr)
+library(tidyr)
+
+new_country_codes <- c("SLO", "CZR", "GFR", "GDR", "YPR", "YAR", "TAW", "TAJ", "SRI", "ANG", "AUL", 
+                       "BRU", "CAO", "CDI", "CRO", "INS", "MZM", "PHI", "BIH", "DDR", "MNE", "ROU", 
+                       "SCG", "SVN", "TLS", "VDR", "YMD", "SUN", "USR", "YUG", "FRY", "MTN", "ROM", 
+                       "CZA", "CSK", "YMS", "YMN", "TMP", "GME", "GMW", "DDR")
+new_codes_df <- data.frame(country_code = new_country_codes)
+cepii.codes <- read.csv('C:/Users/yegor/Desktop/BaranovskiRA_work/DataClean/cleaned.cepii.codes.csv')
+cepii.codes <- cepii.codes[,-2]
+cepii.codes <- c(cepii.codes, new_country_codes)
+
+gdelt.type <- fread('C:/Users/yegor/Desktop/BaranovskiRA_work/DataRAW/GDELT_pre2024.csv')
+match.values.all <- (gdelt.type$Actor1CountryCode %in% cepii.codes) & (gdelt.type$Actor2CountryCode %in% cepii.codes)
+matched.all <- gdelt.type[(match.values.all), ]
+
+# Count occurrences
+df_count <- matched.all %>%
+  group_by(Actor1CountryCode, Actor2CountryCode, Year, EventCode) %>%
+  tally(name = "Count")
+# Create a complete grid
+df_complete <- df_count %>%
+  ungroup() %>%
+  complete(Actor1CountryCode, Actor2CountryCode, Year, EventCode, fill = list(Count = 0))
+
+
+v1_codes <- c("057", "061", "062", "160", "042", "043", "031", "032", "060", "121", "0311", "0312", "1211", "1212")
+v2_codes <- c("057", "061", "062", "160", "042", "043")
+v3_codes <- c("057", "061", "062")
+econ_codes <- c("061", "0311", "1211")
+mil_codes <- c("062", "0312", "1212")
+
+gdelt<-df_complete
+
+gdelt$v1 <- as.numeric(gdelt$EventCode %in% v1_codes)
+gdelt$v2 <- as.numeric(gdelt$EventCode %in% v2_codes)
+gdelt$v3 <- as.numeric(gdelt$EventCode %in% v3_codes)
+gdelt$ECON <- as.numeric(gdelt$EventCode %in% econ_codes)
+gdelt$MIL <- as.numeric(gdelt$EventCode %in% mil_codes)
+
+gdelt<- rename(gdelt, "cameocode" = "EventCode")
+gdelt<- rename(gdelt, "a_ccodealp" = "Actor1CountryCode")
+gdelt<- rename(gdelt, "b_ccodealp" = "Actor2CountryCode")
+
+fwrite(gdelt, 'C:/Users/yegor/Desktop/BaranovskiRA_work/DataClean/CleanedGDELT.csv')
+
+
+
+
+
+
